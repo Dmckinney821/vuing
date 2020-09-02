bf0d97bb764b2785c076bdd5385f91fefc94dd41
Dan McKinney <3dhdaniel@gmail.com>
2020-09-02 16:42:39 -0400

    done

--- final/src/components/stocks/Stock.vue
+++ final/src/components/stocks/Stock.vue
@@ -14,19 +14,26 @@
                         type="number"
                         class='form-control'
                         placeholder='Quantity'
-                        v-model='quantity'>
+                        v-model='quantity'
+                        :class='{danger: insufficientFunds}'>
                 </div>
                 <div class="pull-right">
                     <button
                         class='btn btn-success'
                         @click='buyStock'
-                        :disabled='quantity <= 0 || !Number.isInteger()'
-                        >Buy</button>
+                        :disabled='insufficientFunds || quantity <= 0'
+                        >{{ insufficientFunds ? 'Insufficient Funds' : 'Buy'}}</button>
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+    .danger{
+        border: 1px solid red;
+    }
+</style>
 
 <script>
 export default {
@@ -34,6 +41,14 @@ export default {
     data() {
         return {
             quantity: 0
+        }
+    },
+    computed:{
+        funds() {
+            return this.$store.getters.funds;
+        },
+        insufficientFunds() {
+            return this.quantity * this.stock.price > this.funds;
         }
     },
     methods: {
@@ -44,6 +59,7 @@ export default {
                 quantity: this.quantity
             };
             console.log(order);
+            this.$store.dispatch('buyStock',order)
             this.quantity = 0;
         }
     }
--- final/src/main.js
+++ final/src/main.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
 import App from './App.vue'
+import VueResource from 'vue-resource';
 
 import { routes } from './routes';
 import store from './store/store';
 
 Vue.use(VueRouter);
+
+
+Vue.filter('currency', (value) => {
+  return '$' + value.toLocaleString();
+})
+
+
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://last-for-today.firebaseio.com/'
 
 const router = new VueRouter({
   mode: 'history',
